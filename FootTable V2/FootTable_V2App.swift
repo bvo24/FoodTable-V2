b7a4d7f64e10fb162fe94b9a698176c84629e719
bvo24 <brianvo2020@gmail.com>
2024-05-28 21:03:09 -0400

    Initial Commit

---
+++ FootTable V2/FootTable_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  FootTable_V2App.swift
+//  FootTable V2
+//
+//  Created by Brian Vo on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct FootTable_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
